ad15004598f30648896357ad0b31b5a868f6baa1
Raju <ashfaqraju27@gmail.com>
2023-05-20 15:42:52 +0600

    employees list saved & deleted handled

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -44,13 +44,22 @@ function verifyJWT(req, res, next){
 
 } 
 
+
 async function run(){
     try{
         const appointmentOptionCollection = client.db('jerinParlour').collection('appointmentOption');
         const bookingsCollection = client.db('jerinParlour').collection('bookings');
         const usersCollection = client.db('jerinParlour').collection('users');
+        const employeesCollection = client.db('jerinParlour').collection('employees');
 
 
+
+        const verifyAdmin = (req,res,next) =>{
+            const email = req.query.email;
+            console.log('inside verify Admin', email);
+        }
+
+        
         // Use aggregate to query multiple collection and then merge data
         app.get('/appointmentOption', async(req, res) =>{
             const date= req.query.date;
@@ -70,6 +79,13 @@ async function run(){
             })
             res.send(options);
         });
+
+        app.get('/appointmentSpecialty', async (req,res) =>{
+            const query= {};
+            const result = await appointmentOptionCollection.find(query).project({name: 1}).toArray();
+            res.send(result)
+        })
+
 
         app.get('/bookings',verifyJWT, async(req, res) =>{
             const email = req.query.email;
@@ -122,10 +138,62 @@ async function run(){
             const query ={};
             const users = await usersCollection.find(query).toArray();
             res.send(users);
-        })
+        });
+
         app.post('/users', async(req, res) =>{
             const user = req.body;
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
+
+        //Admin Access handling
+
+        app.get('/users/admin/:email', async (req, res) =>{
+            const email = req.params.email;
+            const query = { email };
+            const user = await usersCollection.findOne(query);
+            res.send({isAdmin: user?.role === 'admin'})
+        })
+
+        app.put('/users/admin/:id',verifyJWT, async(req, res) =>{
+            const decodedEmail = req.decoded.email;
+            const query = {email: decodedEmail};
+            const user = await usersCollection.findOne(query);
+            if(user?.role !== 'admin'){
+                return res.status(403).send({message: 'forbidden access'})
+            }
+            
+            
+            const id = req.params.id;
+            const filter = {_id: new ObjectId(id)};
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result)
+        })
+
+        //employees information 
+        
+        app.get('/employees', async (req, res) =>{
+            const query = {};
+            const result = await employeesCollection.find(query).toArray();
+            res.send(result);
+        })
+
+        app.delete('/employees/:id',verifyJWT, async(req, res) =>{
+            const id = req.params.id;
+            const filter = { _id: new ObjectId(id) };
+            const result = await employeesCollection.deleteOne(filter);
+            res.send(result);
+        })
+
+        app.post('/employees',verifyJWT, async(req, res) =>{
+            const employee = req.body;
+            const result = await employeesCollection.insertOne(employee);
             res.send(result);
         })
     }
